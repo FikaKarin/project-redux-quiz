c100efa2408bdb0beb53c16bb7e4d49596a0d6e5
FikaKarin <mailtolundqvist@gmail.com>
2023-11-14 10:27:03 +0100

    feat: Score counter. Condition to less than half the question length or less to end quiz.

--- src/components/CurrentQuestion/CurrentQuestion.jsx
+++ src/components/CurrentQuestion/CurrentQuestion.jsx
@@ -1,4 +1,3 @@
-// components/CurrentQuestion.jsx
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { quiz } from '../../reducers/quiz';
@@ -6,37 +5,48 @@ import './style.css';
 
 export const CurrentQuestion = () => {
   const dispatch = useDispatch();
-  const { currentQuestionIndex, quizOver, questions, answers } = useSelector(
-    (state) => state.quiz
-  );
+  const { currentQuestionIndex, quizOver, questions, answers, score } =
+    useSelector((state) => state.quiz);
 
   const [selectedIncorrectly, setSelectedIncorrectly] = useState(false);
   const [showCorrectAnswerEffect, setShowCorrectAnswerEffect] = useState(false);
   const [progress, setProgress] = useState(0);
 
-  useEffect(() => {
-    const calculatedProgress = (currentQuestionIndex / questions.length) * 100;
-    setProgress(calculatedProgress);
+useEffect(() => {
+  const calculatedProgress = (currentQuestionIndex / questions.length) * 100;
+  setProgress(calculatedProgress);
+
+  // Check if the questions container is available
+  const questionsContainer = document.querySelector('.questions-container');
+  if (questionsContainer) {
     // Reset the fadeInDown animation and selectedIncorrectly state on each question change
-    const questionsContainer = document.querySelector('.questions-container');
     questionsContainer.classList.remove('fadeInDown');
     void questionsContainer.offsetWidth; // Trigger reflow
     questionsContainer.classList.add('fadeInDown');
-    setSelectedIncorrectly(false);
+  }
 
-    // Reset the correct answer effect state
-    setShowCorrectAnswerEffect(false);
-  }, [currentQuestionIndex]);
+  setSelectedIncorrectly(false);
+
+  // Reset the correct answer effect state
+  setShowCorrectAnswerEffect(false);
+}, [currentQuestionIndex]);
+
 
   if (quizOver) {
     const correctAnswers = answers.filter((answer) => answer.isCorrect);
     const incorrectAnswers = answers.filter((answer) => !answer.isCorrect);
 
+    // Use the score from the state
     return (
       <div className='summary-container'>
         <h1>Quiz Summary</h1>
         <p>Correct Answers: {correctAnswers.length}</p>
         <p>Incorrect Answers: {incorrectAnswers.length}</p>
+        {score < 4 ? (
+          <p>Your score is below 4, you lost!</p>
+        ) : (
+          <p>Congratulations, you won!</p>
+        )}
         <button onClick={() => dispatch(quiz.actions.restart())}>
           Restart Quiz
         </button>
@@ -53,20 +63,30 @@ export const CurrentQuestion = () => {
   const handleAnswerSubmit = (answerIndex) => {
     const isCorrect = question.correctAnswerIndex === answerIndex;
     setSelectedIncorrectly(!isCorrect); // Update state based on correctness
-
+  
     if (isCorrect) {
       // Show correct answer effect for a brief moment
       setShowCorrectAnswerEffect(true);
       setTimeout(() => setShowCorrectAnswerEffect(false), 500); // Adjust duration as needed
     }
+  
+    dispatch(
+      quiz.actions.submitAnswer({
+        questionId: question.id,
+        answerIndex,
+        isCorrect,
+      })
+    );
+  
+    dispatch(quiz.actions.goToNextQuestion());
+  
+    // Check if it's the last question to end the quiz
+    if (currentQuestionIndex + 1 === questions.length) {
+      dispatch(quiz.actions.quizOver()); // Dispatch the action to end the quiz
+    }
+  };    
 
-    setTimeout(() => {
-      dispatch(
-        quiz.actions.submitAnswer({ questionId: question.id, answerIndex })
-      );
-      dispatch(quiz.actions.goToNextQuestion());
-    }, 1000); // Delay for 1 second (adjust as needed)
-  };
+  console.log(score);
 
   return (
     <div
@@ -79,10 +99,11 @@ export const CurrentQuestion = () => {
         <div className='progress-bar' style={{ width: `${progress}%` }}></div>
       </div>
       <h1>
+        <p>Score: {score}</p>
         Question {currentQuestionIndex + 1} / {questions.length}
       </h1>
-      <div className="content-container">
-        <div className="buttons-container">
+      <div className='content-container'>
+        <div className='buttons-container'>
           <h2>{question.questionText}</h2>
           <ul>
             {question.options.map((option, index) => (
@@ -101,7 +122,7 @@ export const CurrentQuestion = () => {
             ))}
           </ul>
         </div>
-        <div className="image-container">
+        <div className='image-container'>
           <img
             src={question.imageURL}
             alt={`Question ${currentQuestionIndex + 1}`}
@@ -109,5 +130,5 @@ export const CurrentQuestion = () => {
         </div>
       </div>
     </div>
-  );  
+  );
 };
--- src/reducers/quiz.js
+++ src/reducers/quiz.js
@@ -1,67 +1,79 @@
-import { createSlice } from "@reduxjs/toolkit";
-import mary from '../assets/mary.png'
-import pinocchio from '../assets/pinocchio.png'
-import lion from '../assets/lion.png'
-import beauty from '../assets/beauty.png'
-import jasmine from '../assets/jasmine.png'
-import bambi from '../assets/bambi.png'
+import { createSlice } from '@reduxjs/toolkit';
+import mary from '../assets/mary.png';
+import pinocchio from '../assets/pinocchio.png';
+import lion from '../assets/lion.png';
+import beauty from '../assets/beauty.png';
+import jasmine from '../assets/jasmine.png';
+import bambi from '../assets/bambi.png';
 
 const questions = [
   {
     id: 1,
-    questionText: "In which Disney movie does a young lion become king of the Pride Lands?",
-    options: ["The Little Mermaid", "Beauty and the Beast", "The Lion King", "Aladdin"],
+    questionText:
+      'In which Disney movie does a young lion become king of the Pride Lands?',
+    options: [
+      'The Little Mermaid',
+      'Beauty and the Beast',
+      'The Lion King',
+      'Aladdin',
+    ],
     correctAnswerIndex: 2,
     imageURL: lion,
   },
   {
     id: 2,
-    questionText: "What is the name of the fairy in Peter Pan?",
-    options: ["Tinker Bell", "Silvermist", "Periwinkle", "Fawn"],
+    questionText: 'What is the name of the fairy in Peter Pan?',
+    options: ['Tinker Bell', 'Silvermist', 'Periwinkle', 'Fawn'],
     correctAnswerIndex: 0,
-    imageURL: "https://pngimg.com/uploads/peter_pan/peter_pan_PNG2.png",
+    imageURL: 'https://pngimg.com/uploads/peter_pan/peter_pan_PNG2.png',
   },
   {
     id: 3,
     questionText: "Who is the villain in 'The Little Mermaid'?",
-    options: ["Ursula", "Maleficent", "Cruella de Vil", "Gaston"],
+    options: ['Ursula', 'Maleficent', 'Cruella de Vil', 'Gaston'],
     correctAnswerIndex: 0,
-    imageURL: "https://pngimg.com/uploads/ariel/ariel_PNG7.png",
+    imageURL: 'https://pngimg.com/uploads/ariel/ariel_PNG7.png',
   },
   {
     id: 4,
     questionText: "What is the name of the magical nanny in 'Mary Poppins'?",
-    options: ["Nanny McPhee", "Mrs. Doubtfire", "Mary Poppins", "Annie"],
+    options: ['Nanny McPhee', 'Mrs. Doubtfire', 'Mary Poppins', 'Annie'],
     correctAnswerIndex: 2,
     imageURL: mary,
   },
   {
     id: 5,
-    questionText: "Which Disney movie features a wooden boy whose nose grows longer when he lies?",
-    options: ["Cinderella", "Snow White and the Seven Dwarfs", "Pinocchio", "Dumbo"],
+    questionText:
+      'Which Disney movie features a wooden boy whose nose grows longer when he lies?',
+    options: [
+      'Cinderella',
+      'Snow White and the Seven Dwarfs',
+      'Pinocchio',
+      'Dumbo',
+    ],
     correctAnswerIndex: 2,
     imageURL: pinocchio,
   },
   {
     id: 6,
     questionText: "What is the name of the princess in 'Beauty and the Beast'?",
-    options: ["Ariel", "Belle", "Jasmine", "Cinderella"],
+    options: ['Ariel', 'Belle', 'Jasmine', 'Cinderella'],
     correctAnswerIndex: 1,
     imageURL: beauty,
   },
   {
     id: 7,
     questionText: "In 'Aladdin', what is the name of the princess?",
-    options: ["Aurora", "Jasmine", "Rapunzel", "Merida"],
+    options: ['Aurora', 'Jasmine', 'Rapunzel', 'Merida'],
     correctAnswerIndex: 1,
     imageURL: jasmine,
   },
   {
     id: 8,
     questionText: "What kind of animal is Thumper in 'Bambi'?",
-    options: ["Rabbit", "Skunk", "Deer", "Squirrel"],
+    options: ['Rabbit', 'Skunk', 'Deer', 'Squirrel'],
     correctAnswerIndex: 0,
-    imageURL: bambi
+    imageURL: bambi,
   },
 ];
 
@@ -69,14 +81,16 @@ const initialState = {
   questions,
   answers: [],
   currentQuestionIndex: 0,
-  quizOver: false
+  quizOver: false,
+  score: 0,
 };
 
 export const quiz = createSlice({
-  
-  name: "quiz",
+  name: 'quiz',
   initialState,
   reducers: {
+    // ... (your existing reducers)
+
     /**
      * Use this action when a user selects an answer to the question.
      * The answer will be stored in the `quiz.answers` state with the
@@ -95,26 +109,34 @@ export const quiz = createSlice({
     submitAnswer: (state, action) => {
       const { questionId, answerIndex, isCorrect } = action.payload;
       const question = state.questions.find((q) => q.id === questionId);
-    
+
       if (!question) {
         throw new Error(
-          "Could not find question! Check to make sure you are passing the question id correctly."
+          'Could not find question! Check to make sure you are passing the question id correctly.'
         );
       }
-    
+
       if (question.options[answerIndex] === undefined) {
         throw new Error(
           `You passed answerIndex ${answerIndex}, but it is not in the possible answers array!`
         );
       }
-    
+
       state.answers.push({
         questionId,
         answerIndex,
         question,
         answer: question.options[answerIndex],
-        isCorrect
+        isCorrect,
       });
+
+      // Update the score based on correctness
+      if (isCorrect) {
+        state.score += 1; // Add a point for correct answers
+      } else {
+        // Deduct points for incorrect answers (you can adjust the value)
+        state.score -= 1;
+      }
     },
 
     /**
@@ -125,10 +147,25 @@ export const quiz = createSlice({
      * This action does not require a payload.
      */
     goToNextQuestion: (state) => {
-      if (state.currentQuestionIndex + 1 === state.questions.length) {
+      if (state.score < 0) {
+        // If the user's score is negative, reset it to 0
+        state.score = 0;
+      }
+
+      // Calculate the threshold for the minimum score required to continue
+      const threshold = Math.ceil(state.questions.length / 2);
+
+      const remainingQuestions =
+        state.questions.length - (state.currentQuestionIndex + 1);
+      if (state.score + remainingQuestions >= threshold) {
+        // If it's possible, go to the next question
+        state.currentQuestionIndex += 1;
+      } else if (state.currentQuestionIndex + 1 === state.questions.length) {
+        // If it's the last question, end the quiz
         state.quizOver = true;
       } else {
-        state.currentQuestionIndex += 1;
+        // If it's not the last question and the score is below the threshold, end the quiz
+        state.quizOver = true;
       }
     },
 
@@ -141,6 +178,17 @@ export const quiz = createSlice({
      */
     restart: () => {
       return initialState;
-    }
-  }
+    },
+
+    /**
+     * Use this action to set the quiz as over. This action should be
+     * dispatched when the user's score is below a certain threshold.
+     *
+     * This action does not require a payload.
+     */
+    quizOver: (state) => {
+      state.quizOver = true;
+      state.currentQuestionIndex = state.questions.length; // Set the index to a value that's out of bounds
+    },
+  },
 });
